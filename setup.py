b7e46f10847f8028a0c725ce67520f7615008eed
Endlesszombiez <30930366+Endlesszombiez@users.noreply.github.com>
2021-11-22 19:28:01 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 import multiprocessing
 from setuptools import setup
 
-setup(name='urbandictionary[1.0]',
-    version='1.0',
+setup(name='urbandictionary-fixed',
+    version='2.0',
     py_modules=['urbandictionary'],
     author='Endlessz',
     author_email='lckrep@protonmail.com',
     license='MIT',
-    description='Simple Python wrapper for Urban Dictionary API.',
+    description='Simple Python wrapper for Urban Dictionary API. Forked and fixed from Bocong',
     url='https://github.com/Endlesszombiez/urbandictionary-py',
     classifiers=[
             "Development Status :: 4 - Beta",
